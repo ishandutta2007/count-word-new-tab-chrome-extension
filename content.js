45a87dd37a05939a76f08633b23b2522d24eb43b
Ishan Dutta <ishandutta2007@gmail.com>
2020-06-28 18:05:59 +0530

    copypastefix

--- content.js
+++ content.js
@@ -1,4 +1,4 @@
-const re = new RegExp(request.toLowerCase());//, 'gi')
+const re = new RegExp("ishan");//, 'gi')
 const matches = document.documentElement.innerHTML.toLowerCase().match(re) || 0
 chrome.runtime.sendMessage({
   url: window.location.href,

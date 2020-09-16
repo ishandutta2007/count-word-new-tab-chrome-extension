712b9e66e4c990eb58b23e9ac53cc65173e303ba
Ishan Dutta <ishandutta2007@gmail.com>
2020-09-16 14:02:16 +0530

    bug_fix

--- content.js
+++ content.js
@@ -1,6 +1,16 @@
-const re = new RegExp("ishan");//, 'gi')
-const matches = document.documentElement.innerHTML.toLowerCase().match(re) || 0
+let matches = document.documentElement.innerHTML.toLowerCase().matchAll("ishan")
+let count = 0
+let n =  false
+let indexes = []
+do{
+    n = matches.next()
+    if(!n.done){
+        indexes.push(n.value.index)
+        count++;
+    }
+}while(!n.done)
+
 chrome.runtime.sendMessage({
   url: window.location.href,
-  count: matches.length
+  count: count
 });
